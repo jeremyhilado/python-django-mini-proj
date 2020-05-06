cfd6a92ac9b1bb57d39fe73f2d35379cb86d0a6f
Jeremy Hilado <jhilado89@gmail.com>
2020-05-06 00:07:53 -0700

    added api

--- musicrest/musicrest/urls.py
+++ musicrest/musicrest/urls.py
@@ -19,5 +19,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('apps.authentication.urls'))
+    path('auth/', include('apps.authentication.urls')),
+    path('api/', include('apps.api.urls'))
 ]
